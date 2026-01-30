e658ddfbcd305ce94b20c5e6b930b490d1b8a52b
Panagiotis <greek.player@hotmail.com>
2026-01-30 19:52:49 +0200

    test

--- demoapp.java
+++ demoapp.java
@@ -17,6 +17,6 @@ public class DemoAppApplication {
 class ProfileController {
     @GetMapping("/profile")
     public String profile() {
-        return "Welcome to the Demo App!\nName: Panagiotis Beis\nRole: DevOps Engineer";
+        return "Welcome to the Demo App!\nName: Panagiotis\nRole: DevOps Engineer";
     }
 }
